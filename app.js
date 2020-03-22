9c05a4cf41ffb03c5ab22c846b9abcc12f047616
nunes-will <william-x7@hotmail.com>
2020-03-22 13:08:00 -0300

    Added function to readdir syncronously and asyncronously

--- app.js
+++ app.js
@@ -12,13 +12,19 @@ const logger = require('./logger');
 // Piece of code to show the total and free memory from the os 
 const os = require('os');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
 
-console.log('Total Memory: ' + totalMemory);
-console.log('Free memory: ' + freeMemory);
+// console.log('Total Memory: ' + totalMemory);
+// console.log('Free memory: ' + freeMemory);
 
-// 
+// Reads the content of the directory syncronously
 const fs = require('fs');
 const files = fs.readdirSync('./');
 console.log(files);
+
+// Reads the content of the directory asyncronously
+fs.readdir('./', function(err, files) {
+    if (err) console.log('Error', err);
+    else console.log('Result: ', files);
+});
